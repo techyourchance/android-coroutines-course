4d7f08cac4f975764509195400c44d45fb7c8e98
Vasiliy Zukanov <techyourchance@gmail.com>
2020-09-11 17:56:42 +0300

    Background thread demonstration

--- app/src/main/java/com/techyourchance/coroutines/home/ScreenReachableFromHome.kt
+++ app/src/main/java/com/techyourchance/coroutines/home/ScreenReachableFromHome.kt
@@ -1,5 +1,6 @@
 package com.techyourchance.coroutines.home
 
 enum class ScreenReachableFromHome(val description: String) {
-    UI_THREAD_DEMONSTRATION("UI Thread Demo");
+    UI_THREAD_DEMO("UI Thread Demo"),
+    BACKGROUND_THREAD_DEMO("Background Thread Demo")
 }
