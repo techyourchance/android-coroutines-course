aa60a3941aec49d9da529cd8b2c1400c2e5cb4de
Vasiliy Zukanov <techyourchance@gmail.com>
2020-09-22 16:04:55 +0300

    Cancellation of all scope's children demo

--- app/src/main/java/com/techyourchance/coroutines/home/ScreenReachableFromHome.kt
+++ app/src/main/java/com/techyourchance/coroutines/home/ScreenReachableFromHome.kt
@@ -8,4 +8,5 @@ enum class ScreenReachableFromHome(val description: String) {
     COROUTINES_CANCELLATION_DEMO("Coroutines Cancellation"),
     EXERCISE_2("Exercise 2"),
     CONCURRENT_COROUTINES_DEMO("Concurrent Coroutines"),
+    SCOPE_CHILDREN_CANCELLATION_DEMO("Scope Children Cancellation"),
 }
