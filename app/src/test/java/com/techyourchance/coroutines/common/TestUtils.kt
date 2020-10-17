1a9c7ac33d0a474e91dc7276513270252b290887
Vasiliy Zukanov <techyourchance@gmail.com>
2020-10-17 11:08:19 +0300

    Coroutines mechanics demo part 1: CoroutineScope, CoroutineContext, Job

---
+++ app/src/test/java/com/techyourchance/coroutines/common/TestUtils.kt
@@ -0,0 +1,24 @@
+package com.techyourchance.coroutines.common
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+
+object TestUtils {
+
+    fun CoroutineScope.printCoroutineScopeInfo() {
+        println()
+        println("CoroutineScope: $this")
+        println("CoroutineContext: ${this.coroutineContext}")
+        println("Job: ${this.coroutineContext[Job]}")
+        println()
+    }
+
+    fun printJobsHierarchy(job: Job, nestLevel: Int = 0) {
+        val indent = "    ".repeat(nestLevel)
+        println("$indent- $job")
+        for (childJob in job.children) {
+            printJobsHierarchy(childJob, nestLevel + 1)
+        }
+    }
+
+}
