994eb0014ee103ec33050ed9600d72bc07a4c114
Vasiliy Zukanov <techyourchance@gmail.com>
2020-10-18 17:19:40 +0300

    Coroutines mechanics demo part 3: withContext function

--- app/src/test/java/com/techyourchance/coroutines/demonstrations/coroutinesmechanics/CoroutinesMechanicsExplorationsTest.kt
+++ app/src/test/java/com/techyourchance/coroutines/demonstrations/coroutinesmechanics/CoroutinesMechanicsExplorationsTest.kt
@@ -2,6 +2,7 @@ package com.techyourchance.coroutines.demonstrations.coroutinesmechanics
 
 import com.techyourchance.coroutines.common.TestUtils
 import com.techyourchance.coroutines.common.TestUtils.printCoroutineScopeInfo
+import com.techyourchance.coroutines.common.TestUtils.printJobsHierarchy
 import kotlinx.coroutines.*
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
@@ -41,4 +42,29 @@ class CoroutinesMechanicsExplorationsTest {
             println("test done")
         }
     }
+
+    @Test
+    fun coroutineScope_withContext() {
+        runBlocking {
+            val scopeJob = Job()
+            val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
+            scope.printCoroutineScopeInfo()
+            val job = scope.launch(CoroutineName("my coroutine") + Dispatchers.Default) {
+                this.printCoroutineScopeInfo()
+                delay(100)
+                withContext(CoroutineName("withContext") + Dispatchers.IO) {
+                    this.printCoroutineScopeInfo()
+                    delay(100)
+                    println("withContext done")
+                }
+                println("coroutine done")
+            }
+            job.join()
+            println("test done")
+        }
+    }
+
+
+
+
 }
