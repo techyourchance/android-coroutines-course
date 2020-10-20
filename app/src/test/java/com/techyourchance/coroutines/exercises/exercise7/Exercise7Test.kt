765617ab72d7edbf44cef508a3c6c32a21842936
Vasiliy Zukanov <techyourchance@gmail.com>
2020-10-20 17:01:18 +0300

    Exercise 7

---
+++ app/src/test/java/com/techyourchance/coroutines/exercises/exercise7/Exercise7Test.kt
@@ -0,0 +1,63 @@
+package com.techyourchance.coroutines.exercises.exercise7
+
+import com.techyourchance.coroutines.common.TestUtils
+import com.techyourchance.coroutines.common.TestUtils.printCoroutineScopeInfo
+import com.techyourchance.coroutines.common.TestUtils.printJobsHierarchy
+import kotlinx.coroutines.*
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Test
+import java.lang.Exception
+import kotlin.coroutines.EmptyCoroutineContext
+
+class Exercise7Test {
+
+    /*
+    Write nested withContext blocks, explore the resulting Job's hierarchy, test cancellation
+    of the outer scope
+     */
+    @Test
+    fun nestedWithContext() {
+        runBlocking {
+            val scopeJob = Job()
+            val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
+
+
+            scopeJob.join()
+            println("test done")
+        }
+    }
+
+    /*
+    Launch new coroutine inside another coroutine, explore the resulting Job's hierarchy, test cancellation
+    of the outer scope, explore structured concurrency
+     */
+    @Test
+    fun nestedLaunchBuilders() {
+        runBlocking {
+            val scopeJob = Job()
+            val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
+
+
+            scopeJob.join()
+            println("test done")
+        }
+    }
+
+    /*
+    Launch new coroutine on "outer scope" inside another coroutine, explore the resulting Job's hierarchy,
+    test cancellation of the outer scope, explore structured concurrency
+     */
+    @Test
+    fun nestedCoroutineInOuterScope() {
+        runBlocking {
+            val scopeJob = Job()
+            val scope = CoroutineScope(scopeJob + CoroutineName("outer scope") + Dispatchers.IO)
+
+
+            scopeJob.join()
+            println("test done")
+        }
+    }
+
+
+}
