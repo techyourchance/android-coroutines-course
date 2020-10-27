09b719e18623dfe82862e5b4ff76e2c1b6728b3b
Vasiliy Zukanov <techyourchance@gmail.com>
2020-10-27 12:24:18 +0200

    Exercise 8

--- app/src/main/java/com/techyourchance/coroutines/home/ScreenReachableFromHome.kt
+++ app/src/main/java/com/techyourchance/coroutines/home/ScreenReachableFromHome.kt
@@ -19,4 +19,5 @@ enum class ScreenReachableFromHome(val description: String) {
     COROUTINES_CANCELLATION_COOPERATIVE_2_DEMO("Coroutines Cancellation Cooperative 2"),
     EXERCISE_6("Exercise 6"),
     NON_CANCELLABLE_DEMO("Non-Cancellable"),
+    EXERCISE_8("Exercise 8"),
 }
