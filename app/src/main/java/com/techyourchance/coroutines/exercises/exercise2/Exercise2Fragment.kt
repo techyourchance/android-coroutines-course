0a97a292e4be2c272b9dd58ca6bd9e96396727e7
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-12-06 18:45:24 -0300

    Solve exercise 2

--- app/src/main/java/com/techyourchance/coroutines/exercises/exercise2/Exercise2Fragment.kt
+++ app/src/main/java/com/techyourchance/coroutines/exercises/exercise2/Exercise2Fragment.kt
@@ -10,19 +10,17 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.techyourchance.coroutines.R
 import com.techyourchance.coroutines.common.BaseFragment
 import com.techyourchance.coroutines.common.ThreadInfoLogger
 import com.techyourchance.coroutines.exercises.exercise1.GetReputationEndpoint
 import com.techyourchance.coroutines.home.ScreenReachableFromHome
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 
 class Exercise2Fragment : BaseFragment() {
 
-    private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
+    private var job: Job? = null
 
     override val screenTitle get() = ScreenReachableFromHome.EXERCISE_2.description
 
@@ -36,12 +34,16 @@ class Exercise2Fragment : BaseFragment() {
         getReputationEndpoint = compositionRoot.getReputationEndpoint
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_exercise_2, container, false)
 
         edtUserId = view.findViewById(R.id.edt_user_id)
         edtUserId.addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {            }
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 btnGetReputation.isEnabled = !s.isNullOrEmpty()
@@ -53,15 +55,22 @@ class Exercise2Fragment : BaseFragment() {
         btnGetReputation = view.findViewById(R.id.btn_get_reputation)
         btnGetReputation.setOnClickListener {
             logThreadInfo("button callback")
-            coroutineScope.launch {
+            lifecycleScope.launch {
                 btnGetReputation.isEnabled = false
                 val reputation = getReputationForUser(edtUserId.text.toString())
-                Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT).show()
+                Toast.makeText(requireContext(), "reputation: $reputation", Toast.LENGTH_SHORT)
+                    .show()
                 btnGetReputation.isEnabled = true
             }
         }
 
         return view
+    }
+
+    override fun onStop() {
+        super.onStop()
+        lifecycleScope.cancel()
+        btnGetReputation.isEnabled = true
     }
 
     private suspend fun getReputationForUser(userId: String): Int {
