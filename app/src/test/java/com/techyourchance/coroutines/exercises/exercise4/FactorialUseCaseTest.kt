df2febff93b574d32905809002d123c96108b31c
Wesley Jonathan Marcolino <wesleyjonatan@gmail.com>
2021-12-11 18:58:40 -0300

    Add solution to exercise 4

--- app/src/test/java/com/techyourchance/coroutines/exercises/exercise4/FactorialUseCaseTest.kt
+++ app/src/test/java/com/techyourchance/coroutines/exercises/exercise4/FactorialUseCaseTest.kt
@@ -1,19 +1,8 @@
 package com.techyourchance.coroutines.exercises.exercise4
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runBlockingTest
-import kotlinx.coroutines.test.setMain
-import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.`is`
-import org.hamcrest.CoreMatchers.instanceOf
-import org.hamcrest.MatcherAssert
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.After
-import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
 import java.math.BigInteger
@@ -28,27 +17,26 @@ class FactorialUseCaseTest {
     }
 
     @Test
-    fun computeFactorial_0_returns1() {
-        runBlocking {
-            // Arrange
-            // Act
-            val result = SUT.computeFactorial(0, 1000)
-            // Assert
-            assertThat((result as FactorialUseCase.Result.Success).result, `is`(BigInteger("1")))
-        }
+    fun computeFactorial_0_returns1() = runBlocking {
+        val result = SUT.computeFactorial(0, 1000)
+        assertThat((result as FactorialUseCase.Result.Success).result, `is`(BigInteger("1")))
     }
 
     @Test
-    fun computeFactorial_1_returns1() {
+    fun computeFactorial_1_returns1() = runBlocking {
+        val result = SUT.computeFactorial(1, 1000)
+        assertThat((result as FactorialUseCase.Result.Success).result, `is`(BigInteger("1")))
     }
 
     @Test
-    fun computeFactorial_10_returnsCorrectAnswer() {
+    fun computeFactorial_10_returnsCorrectAnswer() = runBlocking {
+        val result = SUT.computeFactorial(10, 1000)
+        assertThat((result as FactorialUseCase.Result.Success).result, `is`(BigInteger("3628800")))
     }
 
     @Test
-    fun computeFactorial_30_returnsCorrectAnswer() {
+    fun computeFactorial_30_returnsCorrectAnswer() = runBlocking {
+        val result = SUT.computeFactorial(30, 1000)
+        assertThat((result as FactorialUseCase.Result.Success).result, `is`(BigInteger("265252859812191058636308480000000")))
     }
-
-
 }
